52df524dd732f3a18ff6bcad4ea2716ba5e94d14
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-02 11:21:46 -0300

    feat: signin validation of email and password

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-    res.send("Hi there, from Sign in!");
-});
+router.post("/api/users/signin",
+    [   
+        body("email")
+            .isEmail()
+            .withMessage("Email must be valid!"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("You must supply a passowrd")
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            throw new RequestValidationError(errors.array());
+        }
+    }
+);
 
 export { router as signinRouter };
