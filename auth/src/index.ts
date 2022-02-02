07526bd420ca249e5cecacee6d2c66302a530d7a
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-01 22:01:43 -0300

    feat: signup password hash and mongo implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter, signinRouter, signupRouter, signoutRouter } from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors";
+
 const app = express();
 
 app.use(json());
@@ -19,6 +22,19 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000!!");
-});
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to MongoDB on GCP")
+    }catch(err) {
+        console.log(err);
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000!!");
+    });
+}
+
+start();
+
