8c837862402e3bfd28916d1ba5e716b5dbbd6fce
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-02 11:22:52 -0300

    feat: jwt key verification on start app method

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,14 +2,21 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session"
 
 import { currentUserRouter, signinRouter, signupRouter, signoutRouter } from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors";
 
 const app = express();
-
+app.set("trust proxy", true)
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -23,6 +30,10 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY){
+        throw new Error("JWT_KEY must be defined!")
+    }
+    
     try {
         await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
         console.log("Connected to MongoDB on GCP")
