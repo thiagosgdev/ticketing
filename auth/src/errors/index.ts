21ede369a149ea89b3a8cfd5de56ef71bfbf4cc0
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-08 13:49:51 -0300

    feat: not authorized error handler

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -2,3 +2,4 @@ export * from "./custom-error";
 export * from "./database-connection-error";
 export * from "./not-found-error";
 export * from "./request-validation-error";
+export * from "./not-authorized-error";
